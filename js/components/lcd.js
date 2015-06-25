4dff35b43fbf4c7a45aab8d7c797405866beb5a6
jffjs <jffreyjs@gmail.com>
2015-06-25 16:54:36 -0400

    do not rerender

--- js/components/lcd.js
+++ js/components/lcd.js
@@ -4,14 +4,17 @@ module.exports = React.createClass({
   getInitialState: function() {
 
     return {
-      screen: new ImageData(160, 144)
+      screen: new ImageData(this.props.gpu.screen, 160, 144)
     };
   },
   compoonentDidMount: function() {
     var ctx = document.getElementById('ldc').getContext('2d');
+    ctx.putImageData(new ImageData(this.state.screen, 160, 144), 0, 0);
+  },
+  shouldComponentUpdate: function() {
+    return false; // don't rerender
   },
   render: function() {
-    this.state.ctx.putImageData(new ImageData(this.state.screen, 160, 144), 0, 0);
 
     return (
         <canvas id="lcd" width="160" height="144">
